d5eb39f4bea0a24cfb8391e30d3eab0534c6cf1f
ShinDSR <danang.wibisono.ft20@mail.umy.ac.id>
2023-05-12 21:29:35 +0700

    first commit

---
+++ lib/view/KategoriBarang/kategori_barang.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:project_api01/controller/kategori_barang_controller.dart';
+import 'package:project_api01/model/kategori_barang_model.dart';
+import 'package:project_api01/view/KategoriBarang/add_kategori_barang.dart';
+
+class KategoriBarang extends StatefulWidget {
+  const KategoriBarang({super.key});
+
+  @override
+  State<KategoriBarang> createState() => _KategoriBarangState();
+}
+
+class _KategoriBarangState extends State<KategoriBarang> {
+  final kategoriBarangController = KategoriBarangController();
+  List<KategoriBarangModel> listKategoriBarang = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void getKategoriBarang() async{
+    final kategoriBarang = await kategoriBarangController.getKategoriBarang();
+    setState(() {
+      listKategoriBarang = kategoriBarang;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Kategori Barang'),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: listKategoriBarang.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                title: Text(listKategoriBarang[index].nama),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.edit),
+                ),
+              ),
+            );
+          },
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder:(context) => const AddKategoriBarang(),));
+          },
+          child: const Icon(Icons.add),
+          ),
+    );
+  }
+}
